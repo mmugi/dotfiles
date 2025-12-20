f732c37424739caca9c467b0b61f9c092f75fba7
mmugi <173437276+mmugi@users.noreply.github.com>
2025-12-20 22:27:24 +0900

    update: 出力するlogレベルの指定を追加

--- libs/bash/log.sh
+++ libs/bash/log.sh
@@ -12,12 +12,23 @@
 # ログのファイル名をフルパスで出力 (true/false)
 : "${LOG_FULLPATH:=false}"
 
+# ログの出力レベル
+#   ERROR : 4
+#   WARN  : 3
+#   NOTICE: 2
+#   INFO  : 1
+#   DEBUG : 0
+# 指定のレベル以上のログは出力されます。
+# 負の値を設定することで、ログ出力を完全に抑制します。
+: "${LOG_LEVEL:=1}"
+
 # stacktraceの出力設定 (true/false)
 # トレース情報のファイル名をフルパスで出力
 : "${LOG_TRACE_FULLPATH:=true}"
 # ログレベルごとのトレース情報を出力の有無
 : "${LOG_TRACE_ERROR:=false}"
 : "${LOG_TRACE_WARN:=false}"
+: "${LOG_TRACE_NOTICE:=false}"
 : "${LOG_TRACE_INFO:=false}"
 : "${LOG_TRACE_DEBUG:=false}"
 
@@ -81,21 +92,31 @@ log::_log_emit() {
 }
 
 log.error() {
+  (( LOG_LEVEL > 4 || LOG_LEVEL < 0 )) && return 0
   log::_log_emit 'ERROR' "$ESC_C_STDERR_CRITICAL" "$*"
   if [[ "$LOG_TRACE_ERROR" == 'true' ]]; then log::_log_stacktrace; fi
 }
 
 log.warn() {
+  (( LOG_LEVEL > 3 || LOG_LEVEL < 0 )) && return 0
   log::_log_emit 'WARN' "$ESC_C_STDERR_WARNING" "$*"
   if [[ "$LOG_TRACE_WARN" == 'true' ]]; then log::_log_stacktrace; fi
 }
 
+log.notice() {
+  (( LOG_LEVEL > 2 || LOG_LEVEL < 0 )) && return 0
+  log::_log_emit 'NOTICE' "$ESC_C_NOTICE" "$*"
+  if [[ "$LOG_TRACE_NOTICE" == 'true' ]]; then log::_log_stacktrace; fi
+}
+
 log.info() {
+  (( LOG_LEVEL > 1 || LOG_LEVEL < 0 )) && return 0
   log::_log_emit 'INFO' "$ESC_C_STDERR_INFO" "$*"
   if [[ "$LOG_TRACE_INFO" == 'true' ]]; then log::_log_stacktrace; fi
 }
 
 log.debug() {
+  (( LOG_LEVEL > 0 || LOG_LEVEL < 0 )) && return 0
   log::_log_emit 'DEBUG' "$ESC_C_STDERR_DEBUG" "$*"
   if [[ "$LOG_TRACE_DEBUG" == 'true' ]]; then log::_log_stacktrace; fi
 }
