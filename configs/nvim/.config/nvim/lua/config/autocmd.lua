b6d6aa927ad2f52d1f93729139f2102053e13d0b
mmugi <173437276+mmugi@users.noreply.github.com>
2026-05-06 03:45:22 +0900

    feat(nvim): trailing whitespaceのハイライトを通常ファイルのみ適用するようにautocmd追加

--- configs/nvim/.config/nvim/lua/config/autocmd.lua
+++ configs/nvim/.config/nvim/lua/config/autocmd.lua
@@ -9,3 +9,26 @@ vim.api.nvim_create_autocmd("WinEnter", {
     vim.opt_local.cursorcolumn = true
   end,
 })
+
+local function update_hl_trailing_whitespace()
+  -- initialize pattern matching
+  if vim.w.hl_trailing_whitespace_match_id then
+    pcall(vim.fn.matchdelete, vim.w.hl_trailing_whitespace_match_id)
+    vim.w.hl_trailing_whitespace_match_id = nil
+  end
+
+  -- normal file buffer only
+  if vim.bo.buftype == "" then
+    vim.w.hl_trailing_whitespace_match_id =
+      vim.fn.matchadd("ErrTrailingWhitespace", [[\s\+$]])
+  end
+end
+
+vim.api.nvim_create_autocmd({
+  "BufEnter",
+  "BufWinEnter",
+  "WinEnter",
+  "TermOpen",
+}, {
+  callback = update_hl_trailing_whitespace,
+})
