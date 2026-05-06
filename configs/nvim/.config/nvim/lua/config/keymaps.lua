9b3108f2985633e028d0c0421bb8eab061868017
mmugi <173437276+mmugi@users.noreply.github.com>
2026-05-06 15:32:49 +0900

    feat(nvim): add keymaps for tab navigation

--- configs/nvim/.config/nvim/lua/config/keymaps.lua
+++ configs/nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set({ 'n', 'x' }, 's', '"_s')
 vim.keymap.set({ 'n', 'x' }, 'S', '"_S')
 vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
 vim.keymap.set({ 'n', 'x' }, 'X', '"_X')
+
+vim.keymap.set('n', '<leader>n', '<cmd>tabnext<cr>')
+vim.keymap.set('n', '<leader>p', '<cmd>tabprevious<cr>')
