8e11aec3bd93b69ce86a407f43548bc808d00004
mmugi <173437276+mmugi@users.noreply.github.com>
2026-05-07 21:08:27 +0900

    feat(theme): テーマローダーおよびテーマファイルの構成を変更; テーマ `mmerr` を追加;

--- lib/bash/theme.sh
+++ lib/bash/theme.sh
@@ -4,187 +4,195 @@ LIB_DEPS=( core escseq termcap )
 [[ "${1:-}" = '__META_PROBE__' ]] && return 0
 
 # USAGE:
-#   theme::loader <themefile>
+#   theme::load <theme>
 #
 # DESCRIPTION:
-#   - themes/<theme_name>.sh をsourceする。
-#   - THEME_PALETTE に定義されるテーマ変数を themefile で定義する。
-#   - THEME_PALETTE の変数が定義されていない場合は読み込みエラーを確認して、
-#     themefile の変数を確認する。
-#   - テーマ変数には指定のカラーのエスケープシーケンス出力を定義する。
-#   - 読み込んだテーマ変数から以下のサフィックスを付与したstdout, stderr用の
-#     変数を定義する。
-#     - THEME_VAR_STDOUT_SUFFIX: _STDOUT
-#     - THEME_VAR_STDERR_SUFFIX: _STDERR
-#   - fd 1(stdout), 2(stderr)がターミナルに接続されているかどうかをチェックし、
-#     接続されていない場合はそれぞれの変数を空にセットする。
-#   - TERMCAP_COLOR_MODE で上記の動作をオーバーライド可能。(termcap.sh参照)
-#     - auto: fdのターミナル接続をチェック
+#   themes/<theme>.sh もしくは themes/<theme>/init.sh をsourceして <theme>::setup() を実行します。
+#
+#   <theme>::setup() で定義されるstyle配列(<theme>::setup() の構成 を参照)をもとに、
+#   THEME_STYLE_MAP_NAME_STDOUT, THEME_STYLE_MAP_NAME_STDERR に示す名前の配列を定義します。
+#   theme::load 実行時の標準出力(fd 1), 標準エラー出力(fd 2)がターミナルに接続されているかどうかを
+#   判定し、接続されていない場合はそれぞれの配列を空にセットします。
+#   TERMCAP_COLOR_MODE で上記の動作をオーバーライド可能です。(termcap.sh参照)
+#     - auto: fdのターミナル接続をチェック (default)
 #     - always: 常にエスケープシーケンスを定義
-#     - never: 常に空で定義 (NO_COLORを定義した場合と同じ動作)
+#     - never: 常に空で定義 (NO_COLORを定義しても同じ動作になります)
 
-readonly THEME_VAR_STDOUT_SUFFIX='_STDOUT'
-readonly THEME_VAR_STDERR_SUFFIX='_STDERR'
-readonly THEME_PALETTE=(
-  THEME_RESET
-  THEME_BOLD
-  THEME_FAINT
-  THEME_ITALIC
-  THEME_UNDERLINE
-  THEME_BLINK
-  THEME_RAPID_BLINK
-  THEME_REVERSE
-  THEME_CONCEAL
-  THEME_STRIKE
-  THEME_DEFAULT_INTENCITY
+#   テーマロード後は、スタイルを適用したい場面にあわせて THEME_STYLE_MAP_NAME_STDOUT もしくは
+#   THEME_STYLE_MAP_NAME_STDERR が示す配列を利用します。
+#   テーマによって値が空になる場合があるため、利用側でfallbackを行うようにしてください。
+#
+#   theme::clear で適用したテーマ(定義した配列)を初期化します。テーマ読み込み後に theme::load を
+#   再実行した場合も一度初期化され、再適用されます。
+#
+#   [<theme>::setup() の構成]
+#     - 次の連想配列がグローバルで定義されればOK
+#
+#       THEME_PALETTE_MAP_NAME と同名の連想配列:
+#         - 使用するカラーコード一覧を定義(RGB形式でも指定可(区切り文字: `:` or `;`))
+#         - 例:
+#              declare -g -A THEME_PALETTE=(
+#                ['white']='#000000'
+#                ['red']='#ff0000'
+#                ['blue']='0;0;255'
+#                ['yellow']='255:255:0'
+#                ...
+#              )
+#
+#       THEME_STYLE_MAP_NAME と同名の連想配列:
+#         - エスケープシーケンスを定義
+#         - 標準のテーマではエスケープシーケンスの出力に escseq.sh を利用
+#         - 例:
+#              declare -g -A THEME_STYLE=(
+#                ['error']="$(escseq::sgr --fg-tc "${THEME_PALETTE['red']}")"
+#                ['info']="$(escseq::sgr --fg-tc "${THEME_PALETTE['blue']}")"
+#                ...
+#              )
+#
 
-  THEME_COLOR_BASE
-  THEME_COLOR_MAIN
-  THEME_COLOR_SUB
-  THEME_COLOR_ACCENT
-  THEME_COLOR_MUTED
+readonly THEME_DEFAULT='mmerr'
+readonly THEME_PALETTE_MAP_NAME='THEME_PALETTE'
+readonly THEME_STYLE_MAP_NAME='THEME_STYLE'
+readonly THEME_STYLE_MAP_NAME_STDOUT='STYLE_STDOUT'
+readonly THEME_STYLE_MAP_NAME_STDERR='STYLE_STDERR'
 
-  THEME_COLOR_FATAL
-  THEME_COLOR_ERROR
-  THEME_COLOR_WARN
-  THEME_COLOR_NOTICE
-  THEME_COLOR_INFO
-  THEME_COLOR_DEBUG
-
-  THEME_COLOR_SUCCESS
-  THEME_COLOR_FAILURE
-
-  THEME_COLOR_HEALTHY
-  THEME_COLOR_UNHEALTHY
-
-  THEME_COLOR_COMPLETE
-  THEME_COLOR_DANGER
+declare -g -rA THEME_STYLE_COMMON=(
+  ['sgr_reset']="$(escseq::sgr --reset)"
+  ['sgr_bold']="$(escseq::sgr --bold)"
+  ['sgr_faint']="$(escseq::sgr --faint)"
+  ['sgr_italic']="$(escseq::sgr --italic)"
+  ['sgr_underline']="$(escseq::sgr --underline)"
+  ['sgr_blink']="$(escseq::sgr --blink)"
+  ['sgr_rapid_blink']="$(escseq::sgr --rapid_blink)"
+  ['sgr_reverse']="$(escseq::sgr --reverse)"
+  ['sgr_conceal']="$(escseq::sgr --conceal)"
+  ['sgr_strike']="$(escseq::sgr --strike)"
+  ['sgr_default_intensity']="$(escseq::sgr --default-intensity)"
 )
 
-THEME_RESET="$(escseq::sgr reset)"
-THEME_BOLD="$(escseq::sgr bold)"
-THEME_FAINT="$(escseq::sgr faint)"
-THEME_ITALIC="$(escseq::sgr italic)"
-THEME_UNDERLINE="$(escseq::sgr underline)"
-THEME_BLINK="$(escseq::sgr blink)"
-THEME_RAPID_BLINK="$(escseq::sgr rapid_blink)"
-THEME_REVERSE="$(escseq::sgr reverse)"
-THEME_CONCEAL="$(escseq::sgr conceal)"
-THEME_STRIKE="$(escseq::sgr strike)"
-THEME_DEFAULT_INTENCITY="$(escseq::sgr default_intencity)"
-
-theme::_verificate() {
-  local missing=false
-  local missing_vars=()
-  local var_stderr
-
-  for var in "${THEME_PALETTE[@]}"; do
-    if [[ ! -v "$var" ]]; then
-      missing=true
-      missing_vars+=( "$var" )
-    fi
-  done
-
-  if [[ "$missing" == 'true' ]]; then
-    core::error "required theme variable not set:" "${missing_vars[@]}"
-    return 1
-  fi
-
-  return 0
+theme::clear() {
+  declare -g -A "${THEME_PALETTE_MAP_NAME}=()"
+  declare -g -A "${THEME_STYLE_MAP_NAME}=()"
+  declare -g -A "${THEME_STYLE_MAP_NAME_STDOUT}=()"
+  declare -g -A "${THEME_STYLE_MAP_NAME_STDERR}=()"
 }
 
-theme::_apply_theme_stdout() {
-  local varname
-  if termcap::is_color_supported 1; then
-    for theme_var in "${THEME_PALETTE[@]}"; do
-      varname="${theme_var}${THEME_VAR_STDOUT_SUFFIX}"
-      printf -v "$varname" '%s' "${!theme_var}"
-    done
-  else
-    for theme_var in "${THEME_PALETTE[@]}"; do
-      varname="${theme_var}${THEME_VAR_STDOUT_SUFFIX}"
-      printf -v "$varname" '%s' ''
-    done
-  fi
-}
+theme::_init_styles() {
+  local fd="$1" init_map_name
 
-theme::_apply_theme_stderr() {
-  local varname
-  if termcap::is_color_supported 2; then
-    for theme_var in "${THEME_PALETTE[@]}"; do
-      varname="${theme_var}${THEME_VAR_STDERR_SUFFIX}"
-      printf -v "$varname" '%s' "${!theme_var}"
-    done
-  else
-    for theme_var in "${THEME_PALETTE[@]}"; do
-      varname="${theme_var}${THEME_VAR_STDERR_SUFFIX}"
-      printf -v "$varname" '%s' ''
-    done
-  fi
-}
-
-theme::_apply_theme() {
-  local fd="${1:-1}"
   case "$fd" in
-    1) theme::_apply_theme_stdout ;;
-    2) theme::_apply_theme_stderr ;;
+    1) init_map_name="$THEME_STYLE_MAP_NAME_STDOUT" ;;
+    2) init_map_name="$THEME_STYLE_MAP_NAME_STDERR" ;;
     *)
       core::error "invalid fd: ${fd}"
       return 1
       ;;
   esac
+
+  local -n style_map="$THEME_STYLE_MAP_NAME"
+  local -n init_map="$init_map_name"
+
+  if termcap::is_color_supported "$fd"; then
+    for key in "${!THEME_STYLE_COMMON[@]}"; do
+      init_map["$key"]="${THEME_STYLE_COMMON["$key"]}"
+    done
+    for key in "${!style_map[@]}"; do
+      init_map["$key"]="${style_map["$key"]}"
+    done
+  else
+    for key in "${!THEME_STYLE_COMMON[@]}"; do
+      unset init_map["$key"]
+    done
+    for key in "${!style_map[@]}"; do
+      unset init_map["$key"]
+    done
+  fi
+}
+
+theme::list_styles() {
+  local reset bold count=0
+  local -n style_map="$THEME_STYLE_MAP_NAME"
+  local -n style_map_stdout="$THEME_STYLE_MAP_NAME_STDOUT"
+  local -n style_map_stderr="$THEME_STYLE_MAP_NAME_STDERR"
+
+  local orig=true all=false stdout=false stderr=false
+  while (( $# > 0 )); do
+    case "$1" in
+      --all)    all=true ;;
+      --stdout) orig=false; stdout=true ;;
+      --stderr) orig=false; stderr=true ;;
+      *)
+        core::error "illegal option: $1"
+        return 1
+    esac
+    shift
+  done
+
+  reset="$(printf '\033[0m')"
+  bold="$(printf '\033[1m')"
+
+  if [[ "$all" == 'true' || "$orig" == 'true' ]]; then
+    printf '%b< %s >%b\n' "$bold" "$THEME_STYLE_MAP_NAME" "$reset"
+    for key in "${!style_map[@]}"; do
+      printf '%-25s %b%s%b\n' "$key" "${style_map["$key"]}" 'TEST MESSAGE' "$reset"
+      count=$(( count+1 ))
+    done
+    printf "%b< %s styles listed >%b\n" "$bold" "$count" "$reset"
+    count=0
+  fi
+
+  if [[ "$all" == 'true' || "$stdout" == 'true' ]]; then
+    printf '%b< %s >%b\n' "$bold" "$THEME_STYLE_MAP_NAME_STDOUT" "$reset"
+    for key in "${!style_map_stdout[@]}"; do
+      printf '%-25s %b%s%b\n' "$key" "${style_map_stdout["$key"]}" 'TEST MESSAGE' "$reset"
+      count=$(( count+1 ))
+    done
+    printf "%b< %s styles listed >%b\n" "$bold" "$count" "$reset"
+    count=0
+  fi
+
+  if [[ "$all" == 'true' || "$stdout" == 'true' ]]; then
+    printf '%b< %s >%b\n' "$bold" "$THEME_STYLE_MAP_NAME_STDERR" "$reset"
+    for key in "${!style_map_stderr[@]}"; do
+      printf '%-25s %b%s%b\n' "$key" "${style_map_stderr["$key"]}" 'TEST MESSAGE' "$reset"
+      count=$(( count+1 ))
+    done
+    printf "%b< %s styles listed >%b\n" "$bold" "$count" "$reset"
+    count=0
+  fi
 }
 
 theme::load() {
   local script_dir
   script_dir="$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")"
 
-  if (( $# == 0 )); then
-    core::error "missing themefile operand: ${theme_file}"
-    return 1
-  elif (( $# > 1 )); then
+  if (( $# > 1 )); then
     core::error "illegal options: $*"
     return 1
   fi
 
-  local theme_name="$1"
-  local theme_file="${script_dir:?}/themes/${theme_name}.sh"
+  local theme="${1:-"$THEME_DEFAULT"}"
+  local theme_dir="${script_dir}/themes"
+  local theme_file
 
-  if [[ ! -f "$theme_file" ]]; then
-    core::error "theme file not found: ${theme_file}"
+  if [[ -f "${theme_dir}/${theme}.sh" ]]; then
+    theme_file="${theme_dir}/${theme}.sh"
+  elif [[ -f "${theme_dir}/${theme}/init.sh" ]]; then
+    theme_file="${theme_dir}/${theme}/init.sh"
+  else
+    core::error "cannot find theme: ${theme}"
     return 1
   fi
 
+  theme::clear
+
   # shellcheck source=/dev/null
   source "$theme_file"
-  if theme::_verificate; then
-    theme::_apply_theme 1
-    theme::_apply_theme 2
-  else
-    core::error "theme file verification failed: ${theme_file}"
-    exit 1
+  if ! "${theme}::setup"; then
+    core::error 'setup failed'
+    return 1
   fi
-}
 
-theme::list_colors() {
-  local reset bold faint
-  local name value
-
-  reset="$(printf '\033[0m')"
-  bold="$(printf '\033[1m')"
-  faint="$(printf '\033[2m')"
-
-  printf '%b%-27s%-27s%s%b\n' \
-    "$bold" 'DEFAULT' 'BOLD' 'FAINT' "$reset"
-  while IFS= read -r name; do
-    value="${!name-}"
-    [[ -z "$value" ]] && continue
-    printf '%b%-27s%b%-27s%b%s%b\n' \
-      "$value" "$name" \
-      "$bold" "$name" \
-      "$faint" "$name" \
-      "$reset"
-  done < <(compgen -A variable 'THEME_COLOR_' \
-             | grep -v -e "$THEME_VAR_STDOUT_SUFFIX" \
-                       -e "$THEME_VAR_STDERR_SUFFIX")
+  theme::_init_styles 1
+  theme::_init_styles 2
 }
---
+++ lib/bash/themes/mmerr.sh
@@ -0,0 +1,36 @@
+# shellcheck shell=bash
+# shellcheck disable=SC2034
+
+mmerr::setup() {
+  declare -g -A THEME_PALETTE=(
+    ['white']='#eff7fe'
+    ['black']='#050a15'
+    ['red']='#ea5950'
+    ['scarlet']='#ff2400'
+    ['pink']='#ec94ad'
+    ['neon_pink']='#e26b8c'
+    ['navy']='#222641'
+    ['muted_blue']='#4f6e8d'
+    ['skyblue']='#7dbbfa'
+    ['blue_purple']='#5f5fff'
+    ['lavender']='#6874b3'
+    ['purple']='#948cf3'
+    ['rebeccapurple']='#663399'
+    ['dark_purple']='#404c8b'
+    ['yellow']='#fcdc00'
+    ['neon_green']='#c7f761'
+    ['mint_green']='#c9f9dc'
+    ['tarquoise']='#0becca'
+    ['gray']='#5e6164'
+  )
+  declare -g -A THEME_STYLE=(
+    ['log_fatal']="$(escseq::sgr --fg-tc "${THEME_PALETTE['scarlet']}")"
+    ['log_error']="$(escseq::sgr --fg-tc "${THEME_PALETTE['red']}")"
+    ['log_warn']="$(escseq::sgr --fg-tc "${THEME_PALETTE['yellow']}")"
+    ['log_info']="$(escseq::sgr --fg-tc "${THEME_PALETTE['skyblue']}")"
+    ['log_debug']="$(escseq::sgr --fg-tc "${THEME_PALETTE['lavender']}")"
+    ['log_timestamp']="$(escseq::sgr --fg-tc "${THEME_PALETTE['purple']}")"
+    ['log_stacktrace_function']="$(escseq::sgr --fg-tc "${THEME_PALETTE['blue_purple']}" --bold)"
+    ['log_stacktrace_location']="$(escseq::sgr --fg-tc "${THEME_PALETTE['lavender']}")"
+  )
+}
